cb088ce5a88fcbdbb1d7eeeffcfb9f2f8e578691
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-09-15 18:59:40 -0300

    feat: private route test and add midleware auth

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 const routes = require('express').Router();
 const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middleware/auth')
 
 routes.get('/', (req, res) => {
   res.send({message : 'Hello World'})
 })
 
 routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware)
+routes.get('/dashboard', (req, res) => {
+  return res.status(200).send()
+})
 
 module.exports = routes
