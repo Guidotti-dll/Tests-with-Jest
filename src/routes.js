9bb8e7a1fee0914c599c67242e2c7ba6d99d51a7
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-09-15 15:14:12 -0300

    feat: config jest

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const routes = require('express').Router();
 const {User} = require('./app/models')
 
-User.create({
-  name: 'Lucas',
-  email: 'lucas@contato.com',
-  password_hash: '1das32d4as3d4aw4ad3w'
-})
+// User.create({
+//   name: 'Lucas',
+//   email: 'lucas@contato.com',
+//   password_hash: '1das32d4as3d4aw4ad3w'
+// })
 
 routes.get('/', (req, res) => {
   res.send({message : 'Hello World'})
