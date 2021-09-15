ef2489c3b0d219b58ba26cb3bd31993df72fc19f
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-09-15 16:37:44 -0300

    feat: frist test with application

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,15 +1,27 @@
 const { User } = require('../../src/app/models')
+const request = require('supertest')
+const app = require('../../src/app')
+const truncate = require('../utils/truncate')
 
 
 describe('Authentication', () => {
-  it('should sum two numbers', async () => {
+  beforeEach( async () => {
+    await truncate()
+  })
+  it('should authenticate with valid credentials', async () => {
     const user = await User.create({
       name: 'Lucas',
       email: 'lucas@contato.com',
-      password_hash: '1das32d4as3d4aw4ad3w'
+      password_hash: '123123'
     })
 
-    console.log(user)
-    expect(user.email).toBe('lucas@contato.com')
+      const response = await request(app)
+        .post('/sessions')
+        .send({
+          email: user.email,
+          password: '123456'
+        })
+
+    expect(response.status).toBe(200)
   })
 })
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,10 @@
 const routes = require('express').Router();
-const {User} = require('./app/models')
-
-// User.create({
-//   name: 'Lucas',
-//   email: 'lucas@contato.com',
-//   password_hash: '1das32d4as3d4aw4ad3w'
-// })
+const SessionController = require('./app/controllers/SessionController')
 
 routes.get('/', (req, res) => {
   res.send({message : 'Hello World'})
 })
+
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes
